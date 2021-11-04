ce7109cb223ff55a88ee91f5b8d285b32318f6a2
Alisher778 <alisher.musurmonov89@gmail.com>
2021-11-04 09:51:10 +0500

    js variables and data types

---
+++ 01.day-js/variables.js
@@ -0,0 +1,29 @@
+// Variables - O'zgaruvchilar
+var long_text = 'dhakjsd khbask fdashj fashkj';
+var age = 56;
+
+// Data types
+// Primitive - number, string, boolean, null, undefined
+var myAge = 12;
+var my_age = 12;
+var myName = 'bob'
+var hasKids = true;
+var isMarried = false;
+
+var currentUser = {
+  isLoggedIn: false,
+  account: null
+};
+
+var currentLoggedInUser = {
+  isLoggedIn: false,
+  account: {
+    first_name: 'sssss',
+    last_name: 'aaaaa',
+    email: 'dsjhdj@mail.ru',
+    password: 'dshdsjds',
+  }
+};
+
+
+// Reference - array, object-(function, null, array)
