0a8e4c331fb2ca5be70b309d9fc6583b20574c77
Alisher778 <alisher.musurmonov89@gmail.com>
2021-11-16 11:25:17 +0500

    Dome practice

--- 04.day-js/index.js
+++ 04.day-js/index.js
@@ -13,6 +13,20 @@ function handleKeyPress(event) {
 function handleFormSubmit(event) {
   console.log(event);
   event.preventDefault();
+  const list = document.getElementById('list');
+  const { name, isStudent, universityName, universityAddress } = document.forms["my-form"];
+
+  const newEl = `
+    <li>
+      <h4>Name: ${name.value}</h4>
+      <h4>Are you student: ${isStudent.value}</h4>
+      <h4>University Name: ${universityName.value}</h4>
+      <h4>University address: ${universityAddress.value}</h4>
+    </li>
+  `;
+  list.innerHTML += newEl;
+  event.target.reset();
+  handleCheckBox()
 }
 
 function handleClick(event) {
@@ -20,4 +34,18 @@ function handleClick(event) {
   event.stopPropagation();
   console.log('Button event', event)
   // return true;
+}
+
+function handleCheckBox() {
+  const wrapperEl = document.querySelector('.university-input');
+  const checked = document.getElementById('student').checked;
+  if (checked) {
+    wrapperEl.classList.add('show')
+  } else {
+    wrapperEl.classList.remove('show');
+    const children = wrapperEl.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].value = '';
+    }
+  }
 }
